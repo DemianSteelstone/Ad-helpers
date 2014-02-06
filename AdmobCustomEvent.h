ccb0da295083dd376aab239cca02e05986ea4dcf
Evgeny Rusanov <bartl@macsoftex.com>
2014-02-06 11:23:28 +0300

    no message

---
+++ AdmobCustomEvent.h
@@ -0,0 +1,13 @@
+//
+//  AdmobCustomEvent.h
+//  VideoDownloader
+//
+//  Created by Evgeny Rusanov on 06.02.14.
+//  Copyright (c) 2014 Macsoftex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdmobCustomEvent : MPBannerCustomEvent
+
+@end
---
+++ AdmobCustomEvent.m
@@ -0,0 +1,13 @@
+//
+//  AdmobCustomEvent.m
+//  VideoDownloader
+//
+//  Created by Evgeny Rusanov on 06.02.14.
+//  Copyright (c) 2014 Macsoftex. All rights reserved.
+//
+
+#import "AdmobCustomEvent.h"
+
+@implementation AdmobCustomEvent
+
+@end
